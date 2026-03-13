da90e6a9b03f117c67db1c2e8ad4eef78551987b
Vivek Jha <jhavivek257@gmail.com>
2026-03-13 14:49:45 +0530

    feat: checkout and cod

--- connection.inc.php
+++ connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","ecom");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
-define('SITE_PATH','http://127.0.0.1/php/ecom/');
+$con=mysqli_connect("localhost","root","Test@123","kidzCare");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/kidzcare/');
+define('SITE_PATH','http://127.0.0.1/kidzcare/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
