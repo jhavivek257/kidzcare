0a52a09f5019b37049bd03c7d1e6843771219ed4
Vivek Jha <jhavivek257@gmail.com>
2026-03-14 00:25:24 +0530

    feat: search and filter

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,10 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 session_start();
-$con=mysqli_connect("localhost","root","Test@123","kidzCare");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/kidzcare/');
-define('SITE_PATH','http://127.0.0.1/kidzcare/');
+$con=mysqli_connect("localhost","root","","ecom");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
+define('SITE_PATH','http://127.0.0.1/php/ecom/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
